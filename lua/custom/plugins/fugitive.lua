45d2d9f53478840838950e9fd97f20d19e5dd8e0
Josh Hart <joshcolehart@gmail.com>
2024-06-27 00:22:07 -0700

    add way to help resolve merge conflicts
    After clicking dv on file it will diff where the middle is the conflict
    and the left and right are the files which caused it. gj will choose
    right side and gf will choose the left.

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -2,5 +2,7 @@ return {
   'tpope/vim-fugitive',
   config = function()
     vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = '[g]it [s]tatus' })
+    vim.keymap.set('n', 'gf', '<cmd>diffget //2<CR>')
+    vim.keymap.set('n', 'gj', '<cmd>diffget //3<CR>')
   end,
 }
