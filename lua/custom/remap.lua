46f1e0b60613d30c9c18a4425fc4171c9cf8be76
Josh Hart <joshcolehart@gmail.com>
2024-07-12 18:20:13 -0700

    chore: add comments for custom remaps

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -1,5 +1,6 @@
--- NOTE: Custom remaps
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = '[p]roject [v]iew' })
+
+-- Move text up and down
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
