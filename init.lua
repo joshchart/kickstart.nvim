806fcb03813a3b08229c53baf84e00cf1401aa74
Josh Hart <joshcolehart@gmail.com>
2024-08-13 11:15:36 -0700

    Update mappings to allign with v3 (which-key)

--- init.lua
+++ init.lua
@@ -187,26 +187,35 @@ require('lazy').setup({
   -- after the plugin has been loaded:
   --  config = function() ... end
 
-  { -- Useful plugin to show you pending keybinds.
+  {
     'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+    event = 'VimEnter',
+    config = function()
+      local wk = require 'which-key'
+      wk.setup()
 
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      -- Adding normal mode mappings
+      wk.add {
+        { '<leader>c', group = '[C]ode' }, -- Group for code-related keybinds
+        { '<leader>d', group = '[D]ocument' }, -- Group for document-related keybinds
+        { '<leader>r', group = '[R]ename' }, -- Group for rename-related keybinds
+        { '<leader>s', group = '[S]earch' }, -- Group for search-related keybinds
+        { '<leader>w', group = '[W]orkspace' }, -- Group for workspace-related keybinds
+        { '<leader>t', group = '[T]oggle' }, -- Group for toggle-related keybinds
+        { '<leader>h', group = 'Git [H]unk' }, -- Group for Git hunk-related keybinds
+        { '<leader>c_', hidden = true }, -- Hidden keymap
+        { '<leader>d_', hidden = true }, -- Hidden keymap
+        { '<leader>r_', hidden = true }, -- Hidden keymap
+        { '<leader>s_', hidden = true }, -- Hidden keymap
+        { '<leader>w_', hidden = true }, -- Hidden keymap
+        { '<leader>t_', hidden = true }, -- Hidden keymap
+        { '<leader>h_', hidden = true }, -- Hidden keymap
       }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
+
+      -- Adding visual mode mappings
+      wk.add {
+        { '<leader>h', desc = 'Git [H]unk', mode = 'v' }, -- Visual mode mapping for Git Hunk
+      }
     end,
   },
 
