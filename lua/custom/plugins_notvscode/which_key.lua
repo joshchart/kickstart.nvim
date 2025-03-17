a632aa1bd0574939bac6aed78cb26ba3aa0d3a3d
Josh Hart <joshcolehart@gmail.com>
2025-03-16 19:29:59 -0700

    not using which key currently

--- lua/custom/plugins_notvscode/which_key.lua
+++ lua/custom/plugins_notvscode/which_key.lua
@@ -14,35 +14,35 @@ return {
   -- after the plugin has been loaded:
   --  config = function() ... end
 
-  {
-    'folke/which-key.nvim',
-    event = 'VimEnter',
-    config = function()
-      local wk = require 'which-key'
-      wk.setup()
-
-      -- Adding normal mode mappings
-      wk.add {
-        { '<leader>c', group = '[C]ode' }, -- Group for code-related keybinds
-        { '<leader>d', group = '[D]ocument' }, -- Group for document-related keybinds
-        { '<leader>r', group = '[R]ename' }, -- Group for rename-related keybinds
-        { '<leader>s', group = '[S]earch' }, -- Group for search-related keybinds
-        { '<leader>w', group = '[W]orkspace' }, -- Group for workspace-related keybinds
-        { '<leader>t', group = '[T]oggle' }, -- Group for toggle-related keybinds
-        { '<leader>h', group = 'Git [H]unk' }, -- Group for Git hunk-related keybinds
-        { '<leader>c_', hidden = true }, -- Hidden keymap
-        { '<leader>d_', hidden = true }, -- Hidden keymap
-        { '<leader>r_', hidden = true }, -- Hidden keymap
-        { '<leader>s_', hidden = true }, -- Hidden keymap
-        { '<leader>w_', hidden = true }, -- Hidden keymap
-        { '<leader>t_', hidden = true }, -- Hidden keymap
-        { '<leader>h_', hidden = true }, -- Hidden keymap
-      }
-
-      -- Adding visual mode mappings
-      wk.add {
-        { '<leader>h', desc = 'Git [H]unk', mode = 'v' }, -- Visual mode mapping for Git Hunk
-      }
-    end,
-  },
+  -- {
+  --   'folke/which-key.nvim',
+  --   event = 'VimEnter',
+  --   config = function()
+  --     local wk = require 'which-key'
+  --     wk.setup()
+  --
+  --     -- Adding normal mode mappings
+  --     wk.add {
+  --       { '<leader>c', group = '[C]ode' }, -- Group for code-related keybinds
+  --       { '<leader>d', group = '[D]ocument' }, -- Group for document-related keybinds
+  --       { '<leader>r', group = '[R]ename' }, -- Group for rename-related keybinds
+  --       { '<leader>s', group = '[S]earch' }, -- Group for search-related keybinds
+  --       { '<leader>w', group = '[W]orkspace' }, -- Group for workspace-related keybinds
+  --       { '<leader>t', group = '[T]oggle' }, -- Group for toggle-related keybinds
+  --       { '<leader>h', group = 'Git [H]unk' }, -- Group for Git hunk-related keybinds
+  --       { '<leader>c_', hidden = true }, -- Hidden keymap
+  --       { '<leader>d_', hidden = true }, -- Hidden keymap
+  --       { '<leader>r_', hidden = true }, -- Hidden keymap
+  --       { '<leader>s_', hidden = true }, -- Hidden keymap
+  --       { '<leader>w_', hidden = true }, -- Hidden keymap
+  --       { '<leader>t_', hidden = true }, -- Hidden keymap
+  --       { '<leader>h_', hidden = true }, -- Hidden keymap
+  --     }
+  --
+  --     -- Adding visual mode mappings
+  --     wk.add {
+  --       { '<leader>h', desc = 'Git [H]unk', mode = 'v' }, -- Visual mode mapping for Git Hunk
+  --     }
+  --   end,
+  -- },
 }
